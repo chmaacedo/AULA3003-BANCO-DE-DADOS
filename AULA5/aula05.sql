90bdac4aad60087f6bb403320bec8eb7e431b21e
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-03 20:51:32 -0300

    Criar tabela no banco de dados

--- AULA5/aula05.sql
+++ AULA5/aula05.sql
@@ -1,11 +1,20 @@
 
--- Exibe todos os bancos de dados criados
+/* Exibe todos os bancos de dados criados
 \l
-
+*/
 
 -- Criar banco de dados novo
 CREATE DATABASE aula06ni;
 
 
--- Conectar ao banco de dados
+/* Conectar ao banco de dados
 \c aula05ni;
+*/
+
+-- Criar tabela no banco de dados
+
+CREATE TABLE curso(
+    id int,
+    nome varchar(50)
+);
+
