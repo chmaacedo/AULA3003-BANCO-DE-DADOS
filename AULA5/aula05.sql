944c46e9eab2062a8feec16a7a315e071c2b53cc
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-03 21:15:15 -0300

    Atualizar todos os dados e deletar

--- AULA5/aula05.sql
+++ AULA5/aula05.sql
@@ -3,6 +3,7 @@
 \l
 */
 
+
 -- Criar banco de dados novo
 CREATE DATABASE aula06ni;
 
@@ -11,15 +12,18 @@ CREATE DATABASE aula06ni;
 \c aula05ni;
 */
 
+
 -- Criar tabela no banco de dados
 CREATE TABLE curso(
     id int,
     nome varchar(50)
 );
 
+
 /* Exibe descrição da tabela e as colunas
 \d curso;
 */
+
 
 -- Inserir dados em uma tabela
 INSERT INTO curso VALUES ( 100, 'Sistemas de informação' );
@@ -28,6 +32,13 @@ INSERT INTO curso VALUES ( 300, 'Administração' );
 INSERT INTO curso VALUES ( 400, 'Direito' );
 INSERT INTO curso VALUES ( 500, 'Engenharia de software' );
 
+
 --DDL
 --Consultar dados em uma tabela
 SELECT * FROM curso;
+
+-- Atualiza todos os dados da tabela
+UPDATE curso SET id = 100, nome = 'Medicina';
+
+-- Deleta todos os dados da tabela curso
+DELETE FROM curso;
